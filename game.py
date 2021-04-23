65b7fc5f79dbe1dea8c648944fdab5bb3b98b16d
Witold Sobieszek <witkas07@gmail.com>
2021-04-23 12:49:37 +0200

    Refactor play().

--- game.py
+++ game.py
@@ -39,24 +39,16 @@ class TicTacToe:
         return False
 
     def is_valid_move(self, move):
-        # Move is valid if input is in range 0-8 and the spot on the board is empty.
+        # Move is valid if input is in range 0-8 and chosen spot on the board is empty.
         if move in range(9) and self.board[move] == " ":
             return True
         else:
             return False
 
     def make_move(self, player):
-        # Uses the Player class to make a move.
-        try:
-            move = player.get_move(self)
-            if not self.is_valid_move(move):
-                raise ValueError
-            # If the move is valid, modify the board
-            self.board[move] = player.letter  
-        except ValueError:
-            if isinstance(player, HumanPlayer):
-                print("Invalid move. Please try again.")  # Display an error message if the player is a human.
-            self.make_move(player) 
+        # Modifies the board after selecting the spot by a player.
+        move = player.get_move(self)
+        self.board[move] = player.letter  
 
     def is_board_full(self):
         if any(spot == " " for spot in self.board):
@@ -67,32 +59,28 @@ class TicTacToe:
     def get_available_spots(self):
         return [i for (i, spot) in enumerate(self.board) if spot == " "]
 
-    def play(self, player1, player2):
+    def play(self, x_player, o_player):
         # Print the indices of the board
         print("\nBoard indices:")
         self.print_board_indices()
-        current_player = player1
+        current_player = x_player
+        # Play until there is a winner or the board is full.
         while self.current_winner == None and not self.is_board_full():
             print()
-            self.print_board(self.board)
-            # If the player is computer, make a brief pause
-            if isinstance(current_player, RandomComputerPlayer):
-                print("Computer is making a move...")
-                time.sleep(0.5)
+            # 1. Current player makes a move
             self.make_move(current_player)
-            # Check if the game is won
+            # 2. Check if the game is won
             if self.is_won():
                 self.current_winner = current_player
-            # Change the player
-            if current_player == player1:
-                current_player = player2
-            else:
-                current_player = player1
-        if self.current_winner != None:
+            # 3. Print the board
             self.print_board(self.board)
+            # 4. Change the player
+            current_player = o_player if current_player == x_player else x_player
+
+        print()
+        if self.current_winner != None:
             print(f"{self.current_winner.letter} is the winner!")
         else:
-            self.print_board(self.board)
             print("It's a tie!")
 
 
--- player.py
+++ player.py
@@ -11,14 +11,23 @@ class HumanPlayer(Player):
 
     def get_move(self, game):
         # User input
-        choice = int(input("Select a square (1-9): "))
-        return choice - 1
+        try:
+            choice = int(input("Select a square (1-9): ")) - 1
+            if not game.is_valid_move(choice):
+                raise ValueError
+            return choice
+        except ValueError:
+            print("Invalid move. Please try again.")
+            return self.get_move(game)
 
 class ComputerPlayer(Player):
     pass
 
 class RandomComputerPlayer(ComputerPlayer): 
     def get_move(self, game):
+        # A brief pause
+        print("Computer is making a move...")
+        time.sleep(0.5)
         # Choose random available spot on the board
         return random.choice(game.get_available_spots())
 
